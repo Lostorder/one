b2ee47d8d49be7f2970604b2573ff279936e5f55
Lostorder <1612407123@qq.com>
2021-06-18 16:19:10 +0800

    first

--- src/test/java/com/dogshop/zhuce.java
+++ src/test/java/com/dogshop/zhuce.java
@@ -9,7 +9,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class zhuce {
+public class Texttwo {
+
     WebDriver driver;
 
     @BeforeMethod
@@ -26,29 +27,115 @@ public class zhuce {
     }
 
     @Test
+    public void select() throws InterruptedException {
+        driver.get("https://www.baidu.com");
+        WebElement element01 = driver.findElement(By.xpath("//*[@id='kw']"));
+        element01.sendKeys("零一");
+        WebElement element02 = driver.findElement(By.id("su"));
+        element02.click();
+
+        Thread.sleep(3000);
+
+        /*WebElement element03 = driver.findElement(By.xpath("//*[@id='1']/h3/a"));
+        element03.click();
+
+        Thread.sleep(3000);*/
+
+        String title = driver.getTitle();
+        Assert.assertEquals(title,"零一_百度搜索");
+
+
+    }
+
+    //@Test
     public void login() throws InterruptedException {
 
         driver.get("http://192.168.159.134:8080/DagShop06/");
-        driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/li[1]/a")).click();
+        WebElement element01 = driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/li[2]/a"));
+        element01.click();
 
-        String[] username = {"admin","bb","cc","dd","ee","ff","gg","hh","ii","jj","kk","ll","mm","nn","oo","pp","qq","rr","ss","tt","uu","vv","ww","xx","yy"};
-        String[] password = {"123","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"};
+        Thread.sleep(3000);
 
-        for (int i = 0; i < 1; i++) {
-            Thread.sleep(1000);
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("admin");
+        WebElement inputPassword3 = driver.findElement(By.id("inputPassword3"));
+        inputPassword3.sendKeys("123");
+        WebElement confirmpwd = driver.findElement(By.id("confirmpwd"));
+        confirmpwd.sendKeys("123");
+        WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/div[9]/div/input"));
 
-            driver.findElement(By.id("username")).sendKeys(username[i]);
-            driver.findElement(By.id("inputPassword3")).sendKeys(password[i]);
-            driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/form[1]/div[5]/div/input")).click();
-            //第二种
-            Thread.sleep(2000);
-            String text = driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/text()")).getText();
-            Assert.assertEquals(text,"你好");
+        Thread.sleep(2000);
 
+        element.click();
 
-            driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/li[2]/a")).click();
-            driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/li[1]/a")).click();
+        Thread.sleep(2000);
+
+        boolean isExist_reg = false;
+        try{
+            driver.findElement(By.xpath("//*[contains(.,'" + "恭喜你,注册成功,请登录邮箱完成激活!" + "')]"));
+            isExist_reg = true;
+        }catch (Exception e){
+            isExist_reg = false;
         }
+
+        Assert.assertEquals(isExist_reg,true);
+
+    }
+
+    //@Test
+    public void login01() throws InterruptedException {
+
+        driver.get("http://192.168.159.134:8080/DagShop06/");
+        WebElement element01 = driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/li[2]/a"));
+        element01.click();
+
+        Thread.sleep(3000);
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("admin");
+        WebElement inputPassword3 = driver.findElement(By.id("inputPassword3"));
+        inputPassword3.sendKeys("123");
+        WebElement confirmpwd = driver.findElement(By.id("confirmpwd"));
+        confirmpwd.sendKeys("123");
+        WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/div[9]/div/input"));
+
+        Thread.sleep(2000);
+
+        element.click();
+
+        Thread.sleep(2000);
+
+        String text = driver.findElement(By.xpath("/html/body/div[1]/div[3]/h3")).getText();
+        Assert.assertEquals(text,"恭喜你,注册成功,请登录邮箱完成激活!");
+
+    }
+
+   // @Test
+    public void login02() throws InterruptedException {
+
+        driver.get("http://192.168.159.134:8080/DagShop06/");
+        WebElement element01 = driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ol/li[2]/a"));
+        element01.click();
+
+        Thread.sleep(3000);
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("admin");
+        WebElement inputPassword3 = driver.findElement(By.id("inputPassword3"));
+        inputPassword3.sendKeys("123");
+        WebElement confirmpwd = driver.findElement(By.id("confirmpwd"));
+        confirmpwd.sendKeys("123");
+        WebElement element = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/div[9]/div/input"));
+
+        Thread.sleep(2000);
+
+        element.click();
+
+        Thread.sleep(2000);
+
+        boolean displayed = driver.findElement(By.xpath("/html/body/div[1]/div[3]/h3")).isDisplayed();
+        System.out.println(displayed);
+        Assert.assertEquals(displayed,true);
 
     }
 }
